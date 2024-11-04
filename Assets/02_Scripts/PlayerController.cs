9a4ed9ecc450026ae577e324106375575e56ca57
LeeJaeHyun <myleje@gmail.com>
2024-11-04 15:54:58 +0900

    .

--- Assets/02_Scripts/PlayerController.cs
+++ Assets/02_Scripts/PlayerController.cs
@@ -16,8 +16,27 @@ public class PlayerController : MonoBehaviour
         h = Input.GetAxis("Horizontal"); // -1.0f ~ 0.0f ~ +1.0f
 
         // transform.position += new Vector3(0, 0, 0.1f);
-        transform.position += Vector3.forward * 0.1f;
+        // transform.position += Vector3.forward * 0.1f;
+
+        transform.Translate(Vector3.forward * 0.01f);
 
         // Debug.Log($"h={h} / v={v}");
     }
 }
+
+/*
+    Vector3.forward = Vector3(0, 0, 1)
+    Vector3.up      = Vector3(0, 1, 0)
+    Vector3.right   = Vector3(1, 0, 0)
+
+    Vector3.one  = Vector3(1, 1, 1)
+    Vector3.zero = Vector3(0, 0, 0)
+*/
+
+
+
+
+
+
+
+
