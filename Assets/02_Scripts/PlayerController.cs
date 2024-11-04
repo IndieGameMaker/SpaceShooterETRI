01ed2d5cf3cae55846d4f6293c5268dbb9d16c25
LeeJaeHyun <myleje@gmail.com>
2024-11-04 15:35:16 +0900

    .

--- Assets/02_Scripts/PlayerController.cs
+++ Assets/02_Scripts/PlayerController.cs
@@ -2,15 +2,40 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // 1회 호출
+    // 비활성화 되어도 실행된다.
+    // 모든 Start 메소드 보다 먼저 호출.
+    void Awake()
     {
-        
+
     }
 
-    // Update is called once per frame
+    void OnEnable()
+    {
+
+    }
+
+    // 1회 호출
+    void Start()
+    {
+
+    }
+
+    // 0.02 sec
+    // 물리엔진 계산주기
+    void FixedUpdate()
+    {
+
+    }
+
+    // 화면을 랜더링하는 주기 (60FPS) 1/60 sec
     void Update()
     {
-        
+
+    }
+
+    void LateUpdate()
+    {
+
     }
 }
