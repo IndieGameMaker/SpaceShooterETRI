acf4f05992f010de9b41d818892a71dd21a8ed69
LeeJaeHyun <myleje@gmail.com>
2024-11-04 15:40:31 +0900

    .

--- Assets/02_Scripts/PlayerController.cs
+++ Assets/02_Scripts/PlayerController.cs
@@ -2,40 +2,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // 1회 호출
-    // 비활성화 되어도 실행된다.
-    // 모든 Start 메소드 보다 먼저 호출.
-    void Awake()
-    {
+    private float h;
+    private float v;
 
-    }
-
-    void OnEnable()
-    {
-
-    }
-
-    // 1회 호출
     void Start()
     {
 
     }
 
-    // 0.02 sec
-    // 물리엔진 계산주기
-    void FixedUpdate()
-    {
-
-    }
-
-    // 화면을 랜더링하는 주기 (60FPS) 1/60 sec
     void Update()
     {
+        v = Input.GetAxis("Vertical"); // Up/Down, W/S -1.0f ~ 0.0f ~ +1.0f
+        h = Input.GetAxis("Horizontal"); // -1.0f ~ 0.0f ~ +1.0f
 
-    }
-
-    void LateUpdate()
-    {
-
+        Debug.Log($"h={h} / v={v}");
     }
 }
