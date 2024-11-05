875776933d2ca30f1c271ae4619753d235f03f0c
LeeJaeHyun <myleje@gmail.com>
2024-11-05 10:21:48 +0900

    .

---
+++ Assets/02_Scripts/WeaponController.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform firePos;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
