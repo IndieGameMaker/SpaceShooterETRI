91a59c9ca0b3bbceaa67d0896ef36e4c7fab3e05
LeeJaeHyun <myleje@gmail.com>
2024-11-05 11:10:42 +0900

    .

--- Assets/02_Scripts/RemoveBullet.cs
+++ Assets/02_Scripts/RemoveBullet.cs
@@ -4,7 +4,8 @@ public class RemoveBullet : MonoBehaviour
 {
     void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.tag == "BULLET")
+        //if (other.gameObject.tag == "BULLET")
+        if (other.collider.CompareTag("BULLET"))
         {
             Destroy(other.gameObject);
         }
